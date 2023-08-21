3a6f42385a515a1b115a2e90fc1379357a7963c1
Pepijn van der Linden <pepijnlinden@gmail.com>
2023-08-21 16:40:45 +0200

    All Vault puzzles are working

--- Assets/_HotelAssets/VR-Homicide-Assets/Scripts/FuseboxPuzzle.cs
+++ Assets/_HotelAssets/VR-Homicide-Assets/Scripts/FuseboxPuzzle.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -10,21 +11,24 @@ public class FuseboxPuzzle : MonoBehaviour
     [SerializeField] private GameObject vfxSparks;
     [SerializeField] private int index = 0;
     [SerializeField] private GameObject alarmLight;
+    [SerializeField] private Material greenAlarmMat;
     [SerializeField] private Material redAlarmMat;
+    private bool isEqual;
 
     public void CheckWire(string wireColor)
     {
-        if (wireList[index].name.Contains(wireColor))
+        if (wireList[index].name.Equals(wireColor))
         {
+            wireList[index].SetActive(false);
             index++;
-            wireList[index].GetComponent<GameObject>().SetActive(false);
-            if(index == 2)
+            if (index == 3)
             {
                 vfxSparks.SetActive(true);
+                alarmLight.GetComponent<Renderer>().material = greenAlarmMat;
                 vaultManager.wireBoxPuzzleCompleted = true;
             }
         }
-        else
+        else if (wireList[index].name.Contains("Yellow") || !wireList[index].name.Equals(wireColor))
         {
             alarmLight.GetComponent<Renderer>().material = redAlarmMat;
         }
