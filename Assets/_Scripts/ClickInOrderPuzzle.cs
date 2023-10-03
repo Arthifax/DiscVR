e8684b554c8210ca0db8542a4e87816ff91114b3
Pepijn van der Linden <pepijnlinden@gmail.com>
2023-10-03 15:47:48 +0200

    Added an alarm to the IT Rooms puzzle

--- Assets/_Scripts/ClickInOrderPuzzle.cs
+++ Assets/_Scripts/ClickInOrderPuzzle.cs
@@ -7,25 +7,32 @@ using UnityEngine.Events;
 public class ClickInOrderPuzzle : MonoBehaviour
 {
     [SerializeField] private List<GameObject> buttonList = new List<GameObject>();
-    [SerializeField] private List<GameObject> alarmLights = new List<GameObject>();
-    [SerializeField] private Material neutralAlarmMat;
-    [SerializeField] private Material greenAlarmMat;
+    [SerializeField] private List<GameObject> progressLights = new List<GameObject>();
+    [SerializeField] private Material neutralLightsMat;
+    [SerializeField] private Material greenLightsmMat;
     [SerializeField] private int index = 0;
     [SerializeField] UnityEvent victoryEvent;
 
     [SerializeField] private TextMeshPro doorStatusText;
 
+    [SerializeField] private GameObject alarmLight;
+
     [SerializeField] private AudioSource playerAudio;
+    [SerializeField] private AudioSource gameMusicSource;
     [SerializeField] private AudioClip correctSound;
     [SerializeField] private AudioClip wrongSound;
+    [SerializeField] private AudioClip gameMusic;
+    [SerializeField] private AudioClip alarmSound;
+    [SerializeField] private float alarmTime = 30f;
+
 
     public void CheckPuzzleButton(string buttonName)
     {
         if (buttonList[index].name.Contains(buttonName)) //if the name of the button you pressed is the same as the name in the index
         {
-            alarmLights[index].GetComponent<Renderer>().material = greenAlarmMat;
+            progressLights[index].GetComponent<Renderer>().material = greenLightsmMat;
             index++;
-            if (index == alarmLights.Count)
+            if (index == progressLights.Count)
             {
                 playerAudio.PlayOneShot(correctSound);
                 doorStatusText.text = "OPEN";
@@ -37,10 +44,28 @@ public class ClickInOrderPuzzle : MonoBehaviour
         {
             index = 0;
             playerAudio.PlayOneShot(wrongSound);
-            for (int i = 0; i < alarmLights.Count; i++)
+            TriggerAlarm();
+            for (int i = 0; i < progressLights.Count; i++)
             {
-                alarmLights[i].GetComponent<Renderer>().material = neutralAlarmMat;
+                progressLights[i].GetComponent<Renderer>().material = neutralLightsMat;
             }
         }
+    }
+
+    public void TriggerAlarm()
+    {
+        alarmLight.SetActive(true);
+        StartCoroutine(PlayAlarmRoutine());
+    }
+
+    private IEnumerator PlayAlarmRoutine()
+    {
+        gameMusicSource.clip = alarmSound;
+        gameMusicSource.Play();
+        yield return new WaitForSeconds(alarmTime);
+        alarmLight.SetActive(false);
+        gameMusicSource.Stop();
+        gameMusicSource.clip = gameMusic;
+        gameMusicSource.Play();
     }
 }
